a8772c6e60a32df39af5bf020018c4dcf7bdaa54
athiragithubworld <athirahrms@gmail.com>
2023-04-15 15:08:39 +0530

    CartProvider.js file

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,21 +1,53 @@
-import React from "react";
+import  { useState } from "react";
 
 import CartContext from "./CartContext";
 
-const cartProvider = (props) => {
-  const CartContext = {
-    PrdtList: [],
+const CartProvider = (props) => {
+
+    const [pdtItems ,setPdtItems] = useState([])
+
+    const addProductToCartHandler = (item) =>{
+        let cartPdt =[...pdtItems];
+        // console.log("pdt123",cartPdt)
+        let hasProduct = false;
+
+        cartPdt.forEach((product) =>{
+            
+            if(product.id === item.id) {
+                hasProduct=true;
+                product.quantityLarge = Number(product.quantityLarge)+ Number(item.quantityLarge)
+                product.quantityMedium = Number(product.quantityMedium)+ Number(item.quantityMedium)
+                product.quantitySmall = Number(product.quantitySmall)+ Number(item.quantitySmall)
+            }
+        })
+
+        if(hasProduct){
+            
+            setPdtItems(cartPdt)
+        }
+        else{
+            setPdtItems((prevPdt) =>{
+                const b =[...prevPdt,item]
+                // console.log("bbb",b)
+                return[...prevPdt,item]
+            })
+        }
+
+
+    }
+
+  const CartContexts = {
+    PrdtList:pdtItems ,
     totalPrice: 0,
-    addProduct: (product) => {},
+    addProduct: addProductToCartHandler,
     quantityLarge: 0,
     quantityMedium: 0,
     quantitySmall: 0,
   };
-
-  return <CartContext.Provider value={CartContext
-  }>
+//   console.log("cartcontext",CartContexts)
+  return <CartContext.Provider value={CartContexts}>
     {props.children}
     </CartContext.Provider>;
 };
 
-export default cartProvider;
+export default CartProvider;
