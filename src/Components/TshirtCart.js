7dc5c4d0124908b1484caac697f3fcf9d4b47b17
athiragithubworld <athirahrms@gmail.com>
2023-06-25 23:54:47 +0530

    TshirtCart js file

--- src/Components/TshirtCart.js
+++ src/Components/TshirtCart.js
@@ -15,28 +15,47 @@ const TshirtCart = (props) => {
   return (
     <form onClick={props.onClose} className={classes.CartForm}>
       {/* <div className={classes["cart-items"]}> */}
-        <ul className={classes["cart-items"]}>
-          {cartcntx.PrdtList.map((item) => {
-            let totalprice = (item.price)*(item.quantityLarge+item.quantityMedium+item.quantitySmall)
-
-            return (
-              <li key={item.id}>
-                {/* const price = item.price.reduce((a,v) => a = Number(a)+ ) */}
-                Shirt: {item.tshirt} - Desc: {item.description} - Price:{" "}
-                {item.price} - L {Number(item.quantityLarge)}- M{" "}
-                {item.quantityMedium} - S {item.quantitySmall} -
-                <span> Total Price : {totalprice}</span>    
-                
-               {/* <span>Total Price : {cartcntx.PrdtList.reduce((a,v) => (a=(Number(v.price)*Number(v.quantityLarge+v.quantityMedium+v.quantitySmall))),0)}</span> */}
-              </li>
+      <ul className={classes["cart-items"]}>
+        {cartcntx.PrdtList.map((item) => {
+          let totalprice =
+            Number(item.billPrice) *
+            Number(
+              Number(item.quantityLarge) +
+                Number(item.quantityMedium) +
+                Number(item.quantitySmall)
             );
-          })}
-        </ul>
+
+          return (
+            <li key={item.id}>
+              {/* const price = item.price.reduce((a,v) => a = Number(a)+ ) */}
+              Shirt: {item.billName} - Desc: {item.billDescription} - Price:{" "}
+              {item.billPrice} - L {Number(item.quantityLarge)}- M{" "}
+              {item.quantityMedium} - S {item.quantitySmall} -
+              <span> Total Price : {totalprice}</span>
+              {/* <span>Total Price : {cartcntx.PrdtList.reduce((a,v) => (a=(Number(v.price)*Number(v.quantityLarge+v.quantityMedium+v.quantitySmall))),0)}</span> */}
+            </li>
+          );
+        })}
+      </ul>
       {/* </div> */}
-      
+
       <div className={classes.total}>
         <span>Total Amount : </span>
-        <span>$ {cartcntx.PrdtList.reduce((a,v) => (a=Number(a)+(Number(v.price)*Number(v.quantityLarge+v.quantityMedium+v.quantitySmall))),0)} </span>
+        <span>
+          ${" "}
+          {cartcntx.PrdtList.reduce(
+            (a, v) =>
+              (a =
+                Number(a) +
+                Number(v.billPrice) *
+                  Number(
+                    Number(v.quantityLarge) +
+                      Number(v.quantityMedium) +
+                      Number(v.quantitySmall)
+                  )),
+            0
+          )}{" "}
+        </span>
       </div>
       <div className={classes.button}>
         <button>Product Order</button>
