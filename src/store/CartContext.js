f7e39401912d691a2c50898311d0f572da36897a
athiragithubworld <athirahrms@gmail.com>
2023-06-25 23:52:50 +0530

    CartContext js file updated

--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const CartContext = React.createContext({
-    PrdtList:[],
-    totalPrice:0,
-    addProduct:(product) => {},
-    quantityLarge:0,
-    quantityMedium:0,
-    quantitySmall:0,
-
-})
+  PrdtList: [],
+  totalPrice: 0,
+  addProduct: (product) => {},
+  quantityLarge: 0,
+  quantityMedium: 0,
+  quantitySmall: 0,
+  productList: [],
+  addShirtProducts: (productitem) => {},
+});
 
 export default CartContext;
